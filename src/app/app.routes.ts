ea87573da4698d0aa37689cb4b112edb3d5c4fe5
Thorsten Diekhof <thorsten.diekhof@ts.berlin>
2023-03-14 08:14:11 +0100

    integration tests out. Doesn't work properly.
    Try again later with a different approach and help.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,23 @@ import { OrganizersRoutes } from '../organizers/organizers.routes.config';
 import { AuthRoutes } from '../auth/auth.routes.config';
 
 export function registerRoutes(app: express.Application, preRoute: string = ''){
+	registerUserRoutes(app,preRoute);
+	registerOrganizersRoutes(app,preRoute);
+	registerAuthRoutes(app,preRoute);
+}
+
+
+export function registerUserRoutes(app: express.Application, preRoute: string = ''){
 	const userRoutes = Container.get(UsersRoutes);
 	userRoutes.configureRoutes(app,preRoute);
+}
+
+export function registerOrganizersRoutes(app: express.Application, preRoute: string = ''){
 	const organizersRoutes = Container.get(OrganizersRoutes);
 	organizersRoutes.configureRoutes(app,preRoute);
+}
+
+export function registerAuthRoutes(app: express.Application, preRoute: string = ''){
 	const authRoutes = Container.get(AuthRoutes);
 	authRoutes.configureRoutes(app,preRoute);
-
 }
