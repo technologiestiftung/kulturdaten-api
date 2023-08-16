f36221cf745e3ff99f89bba104676fca08087eef
Thorsten Diekhof <thorsten.diekhof@ts.berlin>
2023-08-16 13:36:37 +0200

    bug: fix SearchLocationsResponse.yml

--- src/generated/faker/faker.SearchLocationsResponse.generated.ts
+++ src/generated/faker/faker.SearchLocationsResponse.generated.ts
@@ -11,16 +11,30 @@
 	import { JSONSchemaFaker, Schema } from 'json-schema-faker';
 	import { SearchLocationsResponse, schemaForSearchLocationsResponse } from "../models/SearchLocationsResponse.generated";
 
-	import { schemaForGetTagsResponse } from '../models/GetTagsResponse.generated';
-	import { schemaForTag } from '../models/Tag.generated';
+	import { schemaForGetLocationsResponse } from '../models/GetLocationsResponse.generated';
+	import { schemaForLocation } from '../models/Location.generated';
 	import { schemaForMetadata } from '../models/Metadata.generated';
+	import { schemaForAddress } from '../models/Address.generated';
+	import { schemaForBorough } from '../models/Borough.generated';
+	import { schemaForCoordinates } from '../models/Coordinates.generated';
+	import { schemaForOpeningHours } from '../models/OpeningHours.generated';
+	import { schemaForExternalLinks } from '../models/ExternalLinks.generated';
+	import { schemaForReference } from '../models/Reference.generated';
+	import { schemaForContact } from '../models/Contact.generated';
 
 	export function fakeSearchLocationsResponse(useExamples: boolean, specifiedPropertiesForSearchLocationsResponse: object = {}): SearchLocationsResponse {
 		const schema = schemaForSearchLocationsResponse as Schema;
 		const refs : Schema[] = [
-			schemaForGetTagsResponse as Schema,
-			schemaForTag as Schema,
+			schemaForGetLocationsResponse as Schema,
+			schemaForLocation as Schema,
 			schemaForMetadata as Schema,
+			schemaForAddress as Schema,
+			schemaForBorough as Schema,
+			schemaForCoordinates as Schema,
+			schemaForOpeningHours as Schema,
+			schemaForExternalLinks as Schema,
+			schemaForReference as Schema,
+			schemaForContact as Schema,
 
 		];
 		JSONSchemaFaker.option('useExamplesValue', useExamples);
--- src/generated/models/SearchLocationsResponse.generated.ts
+++ src/generated/models/SearchLocationsResponse.generated.ts
@@ -11,19 +11,33 @@
 import Ajv, {ValidateFunction} from "ajv";
 import addFormats from "ajv-formats";
 
-import {GetTagsResponse, schemaForGetTagsResponse} from "./GetTagsResponse.generated";
-import {Tag, schemaForTag} from "./Tag.generated";
+import {GetLocationsResponse, schemaForGetLocationsResponse} from "./GetLocationsResponse.generated";
+import {Location, schemaForLocation} from "./Location.generated";
 import {Metadata, schemaForMetadata} from "./Metadata.generated";
+import {Address, schemaForAddress} from "./Address.generated";
+import {Borough, schemaForBorough} from "./Borough.generated";
+import {Coordinates, schemaForCoordinates} from "./Coordinates.generated";
+import {OpeningHours, schemaForOpeningHours} from "./OpeningHours.generated";
+import {ExternalLinks, schemaForExternalLinks} from "./ExternalLinks.generated";
+import {Reference, schemaForReference} from "./Reference.generated";
+import {Contact, schemaForContact} from "./Contact.generated";
 
-export const schemaForSearchLocationsResponse = {$id: "SearchLocationsResponse.yml", $ref: "GetTagsResponse.yml"};
+export const schemaForSearchLocationsResponse = {$id: "SearchLocationsResponse.yml", $ref: "GetLocationsResponse.yml"};
 
 export function validateSearchLocationsResponse(o: object): {isValid: boolean; validate: ValidateFunction} {
   const ajv = new Ajv();
   addFormats(ajv);
   ajv.addKeyword("example");
-  ajv.addSchema(schemaForGetTagsResponse, "GetTagsResponse.yml");
-  ajv.addSchema(schemaForTag, "Tag.yml");
+  ajv.addSchema(schemaForGetLocationsResponse, "GetLocationsResponse.yml");
+  ajv.addSchema(schemaForLocation, "Location.yml");
   ajv.addSchema(schemaForMetadata, "Metadata.yml");
+  ajv.addSchema(schemaForAddress, "Address.yml");
+  ajv.addSchema(schemaForBorough, "Borough.yml");
+  ajv.addSchema(schemaForCoordinates, "Coordinates.yml");
+  ajv.addSchema(schemaForOpeningHours, "OpeningHours.yml");
+  ajv.addSchema(schemaForExternalLinks, "ExternalLinks.yml");
+  ajv.addSchema(schemaForReference, "Reference.yml");
+  ajv.addSchema(schemaForContact, "Contact.yml");
 
   const validate = ajv.compile(schemaForSearchLocationsResponse);
   return {isValid: validate(o), validate: validate};
@@ -33,6 +47,10 @@ export interface SearchLocationsResponse {
   success: boolean;
   message?: string;
   data?: {
-    tags?: Tag[];
+    page?: number;
+    pageSize?: number;
+    totalCount?: number;
+    locations?: Location[];
+    locationsReferences?: Reference[];
   };
 }
