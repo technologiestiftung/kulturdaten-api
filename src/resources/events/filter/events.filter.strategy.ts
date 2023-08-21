a78d308a67fb0047759ba873dc8237c2fea7a073
Thorsten Diekhof <thorsten.diekhof@ts.berlin>
2023-08-21 10:24:50 +0200

    Reference to manual import in the app.ts  to the DI container removed

--- src/resources/events/filter/events.filter.strategy.ts
+++ src/resources/events/filter/events.filter.strategy.ts
@@ -3,17 +3,6 @@ import { SearchEventsRequest } from "../../../generated/models/SearchEventsReque
 import { Event } from '../../../generated/models/Event.generated';
 
 
-/**
- * IMPORTANT: When implementing this interface, ensure to import the implementation
- * into the Dependency Injection (DI) container using the following:
- * 
- * extend the call 
- * `Container.import([<ImplementationName>, <OtherImplementations>]);`
- * in the initDependencyInjetion() method in app.ts
- * 
- * Failure to do so may result in unresolved dependencies during runtime.
- */
-
 export interface EventFilterStrategy {
 	isExecutable(searchEventsRequest:SearchEventsRequest) : boolean;
 	executeRequest(searchEventsRequest:SearchEventsRequest, page:number, pageSize:number) : Promise<{events:Event[], page:number, pageSize:number, totalCount:number}>
