e9469a801a36bd14875e35169b872aa008f2109c
Urs Schmidt <urs.schmidt@ts.berlin>
2023-09-04 16:46:39 +0200

    fix: rename import name

--- src/resources/events/filter/implementations/FindInTheFutureEventsFilterStrategy.ts
+++ src/resources/events/filter/implementations/FindInTheFutureEventsFilterStrategy.ts
@@ -3,12 +3,12 @@ import { Event } from "../../../../generated/models/Event.generated";
 import { SearchEventsRequest } from "../../../../generated/models/SearchEventsRequest.generated";
 import { EventsRepository } from "../../repositories/EventsRepository";
 import { EventFilterStrategy, EventFilterStrategyToken } from "../EventFilterStrategy";
-import { getNow } from "../../../../utils/DateTimeUtil";
+import { getTodayAsString } from "../../../../utils/DateTimeUtil";
 
 @Service({ id: EventFilterStrategyToken, multiple: true })
 export class FindInTheFutureEventsFilterStrategy implements EventFilterStrategy {
 	constructor(@Inject("EventsRepository") public eventsRepository: EventsRepository) {}
-	public today = getNow;
+	public today = getTodayAsString;
 	async executeRequest(searchEventsRequest: SearchEventsRequest): Promise<Event[]> {
 		const filter = searchEventsRequest.inTheFuture
 			? {
