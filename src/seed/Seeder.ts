1a5427e6baa31d60c6f5f3d368c1da52b3fda60f
Thorsten Diekhof <thorsten.diekhof@ts.berlin>
2023-09-06 12:25:43 +0200

    feat: seeder implemented

--- src/seed/Seeder.ts
+++ src/seed/Seeder.ts
@@ -1,91 +1,170 @@
-import argon2 from "argon2";
-
-import { Command } from "commander";
 import { MongoClient } from "mongodb";
 import { MongoDBConnector } from "../common/services/MongoDBConnector";
 import { PermissionFlag } from "../resources/auth/middleware/PermissionFlag";
 import { generateID } from "../utils/IDUtil";
+import * as argon2 from "argon2";
+import { Command } from "commander";
+import tagsJSON from "./tags.json";
+import accessibilityTagsJSON from "./accessibility.json";
 
-class Seeder {
-	public mongoDBConnector: MongoDBConnector | null = null;
+var mongoClient: MongoClient;
+var mongoDBConnector: MongoDBConnector;
 
-	public async initDatabase() {
-		const uri = process.env.MONGO_URI || "mongodb://localhost:27017";
-		const mongoClient = new MongoClient(uri);
+/**
+ * Database Initialization Script
+ * 
+ * ---------------------------
+ * Accessing Help:
+ * ---------------------------
+ * To view a list of available commands and their descriptions, use the `-h` or `--help` flag.
+ * Example: 
+ *  npm run seed -- -h
+ * 
+ * ---------------------------
+ * Adding Tags:
+ * ---------------------------
+ * To add tags to the database, utilize the `-t` or `--tags` flag.
+ * Example: 
+ *  npm run seed -- --tags
+ * 
+ * ---------------------------
+ * Creating an Admin User:
+ * ---------------------------
+ * To introduce an admin user to the database, apply the `-a` or `--admin` flag. 
+ * This should be followed by the user's email and password, structured as `email:password`.
+ * Example: 
+ *  npm run seed -- --admin admin@example.com:password123
+ * 
+ * ---------------------------
+ * Combined Usage:
+ * ---------------------------
+ * It's possible to leverage both functionalities within a single command execution.
+ * Example:
+ *  npm run seed -- --tags --admin admin@example.com:password123
+ * 
+ * ---------------------------
+ * Important Note on Script Behavior:
+ * ---------------------------
+ * This script is designed to populate only empty collections within the database. 
+ * If a collection already contains data, the script will skip adding new entries to prevent duplicates or unintended overwrites.
+ * 
+ * For security reasons, especially with admin user creation, this behavior ensures that no admin user can be programmatically added 
+ * if there are already existing users in the system. This minimizes the risk of unauthorized admin creation or potential misuse.
+ * 
+ */
 
-		const mongoDBConnector = new MongoDBConnector(mongoClient);
-		await mongoDBConnector.initIndex();
+async function main() {
+	const program = new Command();
 
-		process.on("exit", async function () {
-			console.log("Connection to MongoDB terminated.");
-			await mongoClient.close();
-		});
+	program
+		.option("-t, --tags", "If no tags are present, the default tags will be written to the DB")
+		.option(
+			"-a, --admin <mailAndPassword>",
+			"If no admin is present in the DB, an admin with the provided email and password will be created. Format: email:password (e.g. admin@example.com:password123)."
+		);
+
+	program.parse(process.argv);
+
+	const options = program.opts();
+
+	await initDatabase();
+
+	if (options.admin) {
+		await handleAdminCreation(options.admin);
 	}
 
-	public async addAdmin(email: string, password: string) {
-		await this.addUserWithPermission(email, password, PermissionFlag.ADMIN);
+	if (options.tags) {
+		await handleTagInsertion();
 	}
 
-	public async addUser(email: string, password: string) {
-		await this.addUserWithPermission(email, password, PermissionFlag.USER);
-	}
-
-	private async addUserWithPermission(email: string, password: string, permission: PermissionFlag) {
-		if (!this.mongoDBConnector) {
-			await this.initDatabase();
-		}
-		if (!this.mongoDBConnector) {
-			console.log("MongoDb not reachable");
-			return;
-		}
-
-		password = await argon2.hash(password);
-		const user = {
-			email: email.toLowerCase(),
-			password: password,
-			permissionFlags: permission,
-			identifier: generateID(),
-		};
-		const users = await this.mongoDBConnector.users();
-		if ((await users.insertOne(user)).acknowledged) {
-			console.log(`User ${user.email} with identifier ${user.identifier} added`);
-		} else {
-			console.log(`Warning: No user with identifier with email ${user.email} added`);
-		}
-	}
-
-	public addTags(): boolean {
-		return false;
+	if (mongoClient) {
+		await mongoClient.close();
+		console.log("Connection to MongoDB closed.");
 	}
 }
 
-const program = new Command();
+async function initDatabase() {
+	const uri = process.env.MONGO_URI || "mongodb://localhost:27017";
+	mongoClient = new MongoClient(uri);
 
-program
-	.option("-t, --tags", "If no tags are present, the default tags will be written to the DB")
-	.option(
-		"-a, --admin <mailAndPassword>",
-		"If no admin is present in the DB, an admin with the provided email and password will be created. Format: email:password (e.g. admin@example.com:password123)."
-	)
-	.option(
-		"-u, --user <mailAndPassword>",
-		"If no user is present in the DB, a user with the provided email and password will be createdFormat: email:password (e.g. user@example.com:password123)."
-	);
+	mongoDBConnector = new MongoDBConnector(mongoClient);
+	await mongoDBConnector.initIndex();
 
-program.parse(process.argv);
-
-const options = program.opts();
-
-const seeder = new Seeder();
-
-if (options.admin) {
-	const [mail, password] = options.admin.split(":");
-	(async () => {
-		await seeder.addAdmin(mail, password);	})();
+	if (!mongoDBConnector) {
+		console.log("MongoDb not reachable");
+		return;
+	} else {
+		console.log("Connection to database established.");
+	}	
 }
 
-if (options.tags) {
-	console.log("Tags option is set");
+async function areTagsAvailable() {
+	const tags  = await mongoDBConnector.tags();
+	return await tags.countDocuments() > 0;
 }
 
-console.log("SEED END");
+async function addTags() {
+	await addTagsToDatabase(tagsJSON, "CategoryTags have been added.");
+}
+
+async function addAccessibilityTags() {
+	await addTagsToDatabase(accessibilityTagsJSON, "AccessibilityTags have been added.");
+}
+
+async function addTagsToDatabase(tagsData: typeof tagsJSON | typeof accessibilityTagsJSON, logMessage: string) {
+    const db  = await mongoDBConnector.getDatabase();
+    const tags = db.collection('tags');
+    await tags.insertMany(tagsData);
+
+    console.log(logMessage);
+}
+
+async function isAdminUserPresent() {
+	const users = await mongoDBConnector.users();
+	const admins = await users.countDocuments({ permissionFlags: PermissionFlag.ADMIN_PERMISSION });
+	return admins > 0;
+}
+
+async function addAdmin(email: string, password: string) {
+	await addUserWithPermission(email, password, PermissionFlag.ADMIN_PERMISSION);
+}
+
+async function addUserWithPermission(email: string, password: string, permission: PermissionFlag) {
+	const hashedPassword = await argon2.hash(password);
+	const user = {
+		email: email.toLowerCase(),
+		password: hashedPassword,
+		permissionFlags: permission,
+		identifier: generateID(),
+	};
+	const users = await mongoDBConnector.users();
+	const result = await users.insertOne(user);
+
+	if (result.acknowledged) {
+		console.log(`User ${user.email} with identifier ${user.identifier} added`);
+	} else {
+		console.log(`Warning: No user with identifier with email ${user.email} added`);
+	}
+}
+
+async function handleAdminCreation(option: string) {
+	if (await isAdminUserPresent()) {
+		console.log("There is already an admin in the database. Therefore, no new admin can be created.");
+		return;
+	}
+	
+	const [mail, password] = option.split(":");
+	await addAdmin(mail, password);
+}
+
+async function handleTagInsertion() {
+	if (await areTagsAvailable()) {
+		console.log("There are already tags in the database. Therefore, no new tags can be created.");
+		return;
+	}
+	
+	await addTags();
+	await addAccessibilityTags();
+}
+
+main();
