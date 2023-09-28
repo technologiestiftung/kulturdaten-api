a556bf134439bca5c7391d012d871f883f5727ba
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-09-28 18:28:08 +0200

    fix: generate identifier server-side and add timestamps when adding tags

--- src/resources/tags/repositories/MongoDBTagsRepository.ts
+++ src/resources/tags/repositories/MongoDBTagsRepository.ts
@@ -3,15 +3,19 @@ import { MongoDBConnector } from "../../../common/services/MongoDBConnector";
 import { CreateTagRequest } from "../../../generated/models/CreateTagRequest.generated";
 import { Filter } from "../../../generated/models/Filter.generated";
 import { Tag } from "../../../generated/models/Tag.generated";
+import { generateTagID } from "../../../utils/IDUtil";
+import { createMetadata } from "../../../utils/MetadataUtil";
 import { TagsRepository } from "./TagsRepository";
 
 @Service()
 export class MongoDBTagsRepository implements TagsRepository {
 	constructor(@Inject("DBClient") private dbConnector: MongoDBConnector) {}
+
 	async searchTags(filter: Filter): Promise<Tag[]> {
 		const tags = await this.dbConnector.tags();
 		return tags.find(filter, { projection: { _id: 0 } }).toArray();
 	}
+
 	async getTags(): Promise<Tag[]> {
 		const tags = await this.dbConnector.tags();
 		return tags.find({}, { projection: { _id: 0 } }).toArray();
@@ -21,6 +25,7 @@ export class MongoDBTagsRepository implements TagsRepository {
 		const tags = await this.dbConnector.tags();
 		return tags.find({}, { projection: { _id: 0 } }).toArray();
 	}
+
 	async getTagByIdentifier(tagId: string): Promise<Tag | null> {
 		const tags = await this.dbConnector.tags();
 		return tags.findOne({ identifier: tagId }, { projection: { _id: 0 } });
@@ -28,11 +33,18 @@ export class MongoDBTagsRepository implements TagsRepository {
 
 	async addTag(createTagRequest: CreateTagRequest): Promise<Tag | null> {
 		const tags = await this.dbConnector.tags();
-		const result = await tags.insertOne(createTagRequest);
-
+		const newTag: Tag = {
+			...createTagRequest,
+			identifier: generateTagID(),
+			metadata: {
+				...createTagRequest.metadata,
+				...createMetadata(),
+			},
+		};
+		const result = await tags.insertOne(newTag);
 		if (!result.acknowledged) {
 			return null;
 		}
-		return createTagRequest as Tag;
+		return newTag;
 	}
 }
