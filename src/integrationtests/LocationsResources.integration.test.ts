ad7d6c2e6aacf639580c7dbbae861c0c4ac6b15b
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-10-02 11:32:40 +0200

    test: add tests for metadata creation and update for locations

--- src/integrationtests/LocationsResources.integration.test.ts
+++ src/integrationtests/LocationsResources.integration.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 
 import { fakeCreateLocationRequest } from "../generated/faker/faker.CreateLocationRequest.generated";
-import { validateLocation } from "../generated/models/Location.generated";
+import { Location, validateLocation } from "../generated/models/Location.generated";
 import { TestEnvironment } from "./integrationtestutils/TestEnvironment";
 import { LOCATION_IDENTIFIER_REG_EX } from "./integrationtestutils/testmatcher";
 
@@ -12,10 +12,12 @@ let env!: TestEnvironment;
 beforeAll(async () => {
 	env = new TestEnvironment();
 	(await env.startServer()).withLocationsRoutes();
+	jest.useFakeTimers({ advanceTimers: true });
 });
 
 afterAll(async () => {
 	await env.stopServer();
+	jest.useRealTimers();
 });
 
 describe("Validate testData", () => {
@@ -50,9 +52,21 @@ describe("Create locations", () => {
 
 		const newLocationID = body.data.locationReference.referenceId;
 		expect(newLocationID).toMatch(LOCATION_IDENTIFIER_REG_EX);
-		const loc = await env.locations.findOne({ identifier: newLocationID });
+		const createdLocation = await env.locations.findOne<Location>({ identifier: newLocationID });
+		expect(createdLocation!.title!.de).toBe("New Location");
+	});
 
-		expect(loc?.title.de).toBe("New Location");
+	it("should create default metadata with a started and updated timestamp / POST /locations", async () => {
+		jest.setSystemTime(new Date("2023-10-01T01:02:03.000Z"));
+		const { body } = await request(env.app)
+			.post(env.LOCATIONS_ROUTE)
+			.set("Authorization", `Bearer ` + env.USER_TOKEN)
+			.send(fakeCreateLocationRequest(false, { title: { de: "New Location" } }));
+		const newLocationID = body.data.locationReference.referenceId;
+		const createdLocation = await env.locations.findOne<Location>({ identifier: newLocationID });
+		const metadata = createdLocation!.metadata!;
+		expect(metadata.created).toBe("2023-10-01T01:02:03.000Z");
+		expect(metadata.updated).toBe("2023-10-01T01:02:03.000Z");
 	});
 });
 
@@ -119,8 +133,24 @@ describe("Update locations", () => {
 			});
 
 		expect(statusCode).toBe(200);
-		const loc = await env.locations.findOne({ identifier: "LOC-12345678" });
-		expect(loc?.title.de).toBe("Neuer Name");
+		const updatedLocation = await env.locations.findOne<Location>({ identifier: "LOC-12345678" });
+		expect(updatedLocation!.title!.de).toBe("Neuer Name");
+	});
+
+	it("should keep the created timestamp and update the updated timestamp of a event / PATCH /locations/existID", async () => {
+		const identifier = "LOC-12345678";
+		const existingLocation = await env.locations.findOne<Location>({ identifier });
+		jest.setSystemTime(new Date("2023-10-23T01:02:03.000Z"));
+		await request(env.app)
+			.patch(env.LOCATIONS_ROUTE + "/" + identifier)
+			.set("Authorization", `Bearer ` + env.USER_TOKEN)
+			.send({
+				title: { de: "Neuer Name" },
+			});
+		const updatedLocation = await env.locations.findOne<Location>({ identifier });
+		const metadata = updatedLocation!.metadata!;
+		expect(metadata.created).toBe(existingLocation!.metadata!.created);
+		expect(metadata.updated).toBe("2023-10-23T01:02:03.000Z");
 	});
 
 	it("should return an error when an invalid ID is provided / PATCH /locations/invalidID", async () => {
