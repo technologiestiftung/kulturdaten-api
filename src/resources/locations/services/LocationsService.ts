7275058c5d7a382569d42a446a72e97e85950be8
Simon Scholler <simon.scholler@ts.berlin>
2023-12-05 16:52:58 +0100

    feat: geolocation lookup logic implemented

--- src/resources/locations/services/LocationsService.ts
+++ src/resources/locations/services/LocationsService.ts
@@ -9,6 +9,8 @@ import { LocationsRepository } from "../repositories/LocationsRepository";
 import { AuthUser } from "../../../generated/models/AuthUser.generated";
 import { isSuperAdmin } from "../../auth/middleware/PermissionFlag";
 import { CreateLocationRequest } from "../../../generated/models/CreateLocationRequest.generated";
+import { fakeCoordinatess } from "../../../generated/faker/faker.Coordinates.generated";
+import { Coordinates } from "../../../generated/models/Coordinates.generated";
 
 @Service()
 export class LocationsService {
@@ -29,11 +31,61 @@ export class LocationsService {
 		if (authUser?.organizationIdentifier) {
 			resource.manager = { referenceType: "type.Organization", referenceId: authUser.organizationIdentifier };
 		}
+		
+		// query geolocation via address if geolocation is null
+		if (!resource.coordinates) {
+			if (resource.address && resource.borough) {		
+				// assemble geolocation request		
+				var queryAddressParameter = resource.address.streetAddress + ' ' + resource.address.postalCode + ' ' + resource.borough + ' ' + resource.address.addressLocality + ' Germany';
+				queryAddressParameter = encodeURIComponent(queryAddressParameter);
+				var apiRequest = "https://geocode.maps.co/search?q=" + queryAddressParameter;				
+				var coordinates = await LocationsService.getLatLon(apiRequest);
+				// create a Coordinates instance with lat / lon and assign it to resource.coordinates
+				if (coordinates) {
+					resource.coordinates = coordinates;
+				}
+				// introduce a delay (throttle) before making the next request (due to the rate limit of the API)
+				await new Promise(resolve => setTimeout(resolve, 1000)); // 1000 milliseconds = 1 second
+			}
+			else {
+				// log that address is unset and thus geolocation cannot be queried				
+				console.warn("Address is unset for location '%s', cannot query geolocation.", resource.title);	
+			}
+		}
+
 		return this.locationsRepository.addLocation(resource, authUser);
 	}
 
 	search(filter: Filter, pagination?: Pagination): Promise<Location[]> {
 		return this.locationsRepository.searchLocations(filter, pagination);
+	}
+
+	static async getLatLon(apiRequest: RequestInfo): Promise<Coordinates | null>{
+		try {
+			const response = await fetch(
+				apiRequest,
+				{
+					method: 'GET'
+				}
+			);
+
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+		
+			const data = await response.json();
+			if (data.size > 1) {
+				// TODO: How do we properly handle this?				
+				console.warn('Got multiple address lookup gelocations %i', data.size);
+			} else if (data.size = 0) {
+				console.warn('Did not find geolocation. Request URL: %s', apiRequest);
+			}
+			return { latitude: data[0]['lat'], longitude: data[0]['lon'] }
+
+		} catch (error) {
+			console.error('Error fetching data:', error);			
+			return null;
+		}		
 	}
 
 	async countLocations(searchFilter?: Filter): Promise<number> {
