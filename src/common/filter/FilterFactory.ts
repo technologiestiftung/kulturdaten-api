29d98b95e71af9b9677af67c10bd58fb86702a05
Thorsten Diekhof <thorsten.diekhof@ts.berlin>
2023-12-12 09:07:20 +0100

    feat: add FilterFactory for MongoDB Filter

---
+++ src/common/filter/FilterFactory.ts
@@ -0,0 +1,41 @@
+import { Filter } from "../../generated/models/Filter.generated";
+
+export interface FilterFactory<T> {
+	createExactMatchFilter<K extends keyof T>(propertyName: K, value: T[K]): Filter;
+
+	createPartialMatchFilter<K extends keyof T>(propertyName: K, value: string): Filter;
+
+	createAnyMatchFilter<K extends keyof T>(propertyName: K, values: string[]): Filter;
+
+	createAllMatchFilter<K extends keyof T>(propertyName: K, values: string[]): Filter;
+
+	combineWithAnd(filters: Filter[]): Filter;
+
+	combineWithOr(filters: Filter[]): Filter;
+}
+
+export class SimpleFilterFactory<T> implements FilterFactory<T> {
+	createExactMatchFilter<K extends keyof T>(propertyName: K, value: T[K]): Filter {
+		return { [propertyName as string]: value };
+	}
+
+	createPartialMatchFilter<K extends keyof T>(propertyName: K, value: string): Filter {
+		return { [propertyName as string]: { $regex: value, $options: "i" } };
+	}
+
+	createAnyMatchFilter<K extends keyof T>(propertyName: K, values: string[]): Filter {
+		return { [propertyName as string]: { $in: values } };
+	}
+
+	createAllMatchFilter<K extends keyof T>(propertyName: K, values: string[]): Filter {
+		return { [propertyName as string]: { $all: values } };
+	}
+
+	combineWithAnd(filters: Filter[]): Filter {
+		return { $and: filters };
+	}
+
+	combineWithOr(filters: Filter[]): Filter {
+		return { $or: filters };
+	}
+}
